8049b44260d486e9a3256c124abafae4e0073547
AiMaster Ala <99422391+712123846456zcj@users.noreply.github.com>
2025-02-21 09:37:23 +0800

    Delete CivitAI_Image_grabber directory

--- CivitAI_Image_grabber/s2.py
+++
@@ -1,4 +0,0 @@
-import os
-print("\n注意：Model version ID Mode模式\n")
-ss=input("【+】请输入你的模型id：")
-os.system(f"python civit_image_downloader.py --timeout=60  --quality=1 --redownload=2  --mode=4   --model_version_id={ss}")
